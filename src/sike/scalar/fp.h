974f82ad39487c78cf1bce7f0742a42a85fd80dc
Hao Cheng <50016026+ulhaocheng@users.noreply.github.com>
2022-06-25 19:39:45 +0200

    add 2-level Karatsuba int mul

--- src/sike/scalar/fp.h
+++ src/sike/scalar/fp.h
@@ -6,5 +6,6 @@
 
 void mp_mul_1w_v0(uint64_t *r, const uint64_t *a, const uint64_t *b);
 void mp_mul_1w_v1(uint64_t *r, const uint64_t *a, const uint64_t *b);
+void mp_mul_1w_v2(uint64_t *r, const uint64_t *a, const uint64_t *b);
 
 #endif

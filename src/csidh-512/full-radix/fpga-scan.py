942056019c1c455d5f4d6c9fbee99b72bd9851ef
phthinh <hung3@e.ntu.edu.sg>
2022-10-04 09:52:05 +0100

    add arty100T pre-built bitstream and fpga-scan mechanisms

---
+++ src/csidh-512/full-radix/fpga-scan.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2021 SCARV project <info@scarv.org>
+#
+# Use of this source code is restricted per the MIT license, a copy of which 
+# can be found at https://opensource.org/licenses/MIT (or should be included 
+# as LICENSE.txt within the associated archive or repository).
+
+import driver, itertools
+
+# =============================================================================
+
+def rv32( args ) :
+  pass
+
+# -----------------------------------------------------------------------------
+
+def rv64( args ) :
+  if ( args.prog ) :
+    driver.program_fpga( args, 'rv64', 'csidh-512', 'full' )
+
+  for TYPE in [ 'RV64_TYPE1', 'RV64_TYPE2', 'RV64_TYPE3' ] :
+    driver.run( args, 'rv64', 'csidh-512', 'full', TYPE, '' )
+# -----------------------------------------------------------------------------
+
+if ( __name__ == '__main__' ) :
+  driver.main( rv32, rv64 )
+
+# =============================================================================
--- src/hw/fpga/script/driver.py
+++ src/hw/fpga/script/driver.py
@@ -8,49 +8,45 @@ import argparse, os, subprocess
 
 # =============================================================================
 
-def run( args, ALG, CONF, ARCH, IMP, NIST_IMP = None ) :
+def run( args, ARCH, ALG, RADIX, TYPE, CONF ) :
   CONF  = ' '.join( [ '-D%s' % ( x ) for x in CONF ] )
 
-  CONF += ( ' ' if ( CONF != '' ) else '' ) + '-DDRIVER_TRIALS_WARN="%d"' % ( args.trials_warm )
-  CONF += ( ' ' if ( CONF != '' ) else '' ) + '-DDRIVER_TRIALS_REAL="%d"' % ( args.trials_real )
+  CONF += ( ' ' if ( CONF != '' ) else '' ) #+ '-DDRIVER_TRIALS_WARN="%d"' % ( args.trials_warm )
+  CONF += ( ' ' if ( CONF != '' ) else '' ) #+ '-DDRIVER_TRIALS_REAL="%d"' % ( args.trials_real )
 
   print( '>>> ALG      = "%s"' % ( ALG      ) )
-  print( '>>> CONF     = "%s"' % ( CONF     ) )
+  print( '>>> RADIX    = "%s"' % ( RADIX    ) )
   print( '>>> ARCH     = "%s"' % ( ARCH     ) )
-  print( '>>>      IMP = "%s"' % (      IMP ) )
-  print( '>>> NIST_IMP = "%s"' % ( NIST_IMP ) )
+  print( '>>> TYPE     = "%s"' % ( TYPE     ) )
+  print( '>>> CONF     = "%s"' % ( CONF     ) )
 
   env = dict()
 
   env[ 'REPO_HOME'    ] = os.environ[ 'REPO_HOME'    ]
-  env[ 'REPO_VERSION' ] = os.environ[ 'REPO_VERSION' ]
+  #env[ 'REPO_VERSION' ] = os.environ[ 'REPO_VERSION' ]
 
   env[ 'PORT'            ] = os.environ[ 'PORT'    ]
   env[ 'RISCV_ROCKET'    ] = os.environ[ 'RISCV_ROCKET'    ]
   env[ 'ROCKETCHIP_REPO' ] = os.environ[ 'ROCKETCHIP_REPO' ]
 
-  if ( NIST_IMP != None ) :
-    env.update( { 'ALG' : ALG, 'FPGA_SWCONF' : CONF, 'ARCH' : ARCH, 'IMP' : IMP, 'NIST_IMP' : NIST_IMP } )
-  else :
-    env.update( { 'ALG' : ALG, 'FPGA_SWCONF' : CONF, 'ARCH' : ARCH, 'IMP' : IMP                        } )
+  env.update( { 'ARCH' : ARCH, 'ALG' : ALG, 'RADIX' : RADIX, 'TYPE' : TYPE, 'FPGA_SWCONF' : CONF } )
 
   subprocess.run( [ 'make', os.path.expandvars( '--directory=${REPO_HOME}/src/hw' ), 'fpga-swclean', 'fpga-run' ], env = env )
 
   print( '<<<'                                )
 
-def program_fpga( args, ALG, ARCH, ISE = 'xalu' ) :
-  print( '>>> ALG = "%s", ARCH = "%s", ISE = "%s"' % ( ALG, ARCH, ISE ) )
+def program_fpga( args, ARCH='rv64', ALG='x25519', RADIX='full' ) :
+  print( '>>> ARCH = "%s", ALG = "%s", RADIX = "%s" ' % ( ARCH, ALG, RADIX ) )
   print( '>>> Program FPGA ...')
 
   env = dict()
   env[ 'PATH'            ] = os.environ[ 'PATH'            ]
   env[ 'HOME'            ] = os.environ[ 'HOME'            ]
   env[ 'REPO_HOME'       ] = os.environ[ 'REPO_HOME'       ]
-  env[ 'REPO_VERSION'    ] = os.environ[ 'REPO_VERSION'    ]
   env[ 'RISCV_ROCKET'    ] = os.environ[ 'RISCV_ROCKET'    ]
   env[ 'ROCKETCHIP_REPO' ] = os.environ[ 'ROCKETCHIP_REPO' ]
 
-  env.update( { 'ALG' : ALG, 'ARCH' : ARCH, 'ISE' : ISE } )
+  env.update( { 'ALG' : ALG, 'ARCH' : ARCH, 'RADIX' : RADIX } )
   subprocess.run( [ 'make', '--quiet', os.path.expandvars( '--directory=${REPO_HOME}/src/hw' ), 'program-fpga' ], env = env, stdout=subprocess.DEVNULL)
   print( '>>> Done.')
 # -----------------------------------------------------------------------------
@@ -61,12 +57,10 @@ def main( rv32, rv64 ) :
   parser.add_argument( '--rv32',        dest = 'rv32',        action = 'store_true',        default = False )
   parser.add_argument( '--rv64',        dest = 'rv64',        action = 'store_true',        default = False )
 
-  parser.add_argument( '--nist',        dest = 'nist',        action = 'store_true',        default = False )
-
   parser.add_argument( '--prog',        dest = 'prog',        action = 'store_true',        default = False )
 
-  parser.add_argument( '--trials-warm', dest = 'trials_warm', action = 'store', type = int, default = 10  )
-  parser.add_argument( '--trials-real', dest = 'trials_real', action = 'store', type = int, default = 50  )
+  #parser.add_argument( '--trials-warm', dest = 'trials_warm', action = 'store', type = int, default = 10  )
+  #parser.add_argument( '--trials-real', dest = 'trials_real', action = 'store', type = int, default = 50  )
 
   args = parser.parse_args()
 
